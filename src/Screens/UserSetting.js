76d25f0292f2a4450416a18705103ff91a5ae007
SYED DILAWAR HUSSAIN <syeddilawar4848@gmail.com>
2023-02-26 11:16:14 +0500

    account acreeen Designed

--- src/Screens/UserSetting.js
+++ src/Screens/UserSetting.js
@@ -1,15 +1,103 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import OrderView from '../Components/OrderView';
 
 const UserSetting = () => {
-    return (
-        <View>
-            <Text style={{fontSize:30,color:"skyblue",textAlign:"center",fontWeight:"bold",letterSpacing:2}}>Settings</Text>
+  const img =
+    'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo-thumbnail.png';
+  const [showCheck, setShowCheck] = useState('');
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 30,
+          color: 'skyblue',
+          textAlign: 'center',
+          fontWeight: 'bold',
+          letterSpacing: 2,
+        }}>
+        Settings
+      </Text>
+
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            style={{width: 100, height: 100, borderRadius: 50}}
+            source={{uri: img}}
+          />
         </View>
-    )
-}
 
-export default UserSetting
+        <View
+          style={{
+            width: '67%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderBottomColor: 'gray',
+            borderBottomWidth: 1,
+          }}>
+          <TextInput
+            maxLength={24}
+            style={{fontSize: 20}}
+            placeholder="Update Full Name"
+            onChangeText={e => setShowCheck(e)}
+          />
+          {showCheck.length > 10 ? (
+            <Icons name="check" color="red" size={30} />
+          ) : (
+            ''
+          )}
+        </View>
+      </View>
 
-const styles = StyleSheet.create({})
+      <View style={{paddingHorizontal: 10}}>
+        <Text
+          style={{
+            fontSize: 23,
+            letterSpacing: 2,
+            color: '#003275',
+            fontWeight: 'bold',
+          }}>
+          Orders
+        </Text>
+      </View>
+
+      <OrderView />
+      <OrderView />
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'green',
+          paddingVertical: 13,
+          borderRadius: 10,
+          alignItems: 'center',
+          marginVertical: 10,
+          
+        }}>
+        <Text
+          style={{
+            fontSize: 18,
+            color: 'white',
+            letterSpacing: 3,
+            fontWeight: 'bold',
+          }}>
+          Logout
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default UserSetting;
+
+const styles = StyleSheet.create({});
